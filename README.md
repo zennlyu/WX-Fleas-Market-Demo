6cb6efece53c37b470d781863c36e344b6a9f895
QANTM <3598267954@qq.com>
2021-08-05 04:20:28 +0800

    readme

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
+# Fleas-Market-VX-Demo
+
 Powered By `微信小程序`,`JWT`, `Java8`,`Maven`,`SpringBoot`,`SpringCloud`,`MySQL`,`Redis`,`RabbitMQ`,`Docker`
+
---
+++ dev/sh.sh
@@ -0,0 +1,13 @@
+cd mysql
+chmod ugo+x shart.sh
+./shart.sh
+cd ..
+cd rabbitmq
+chmod ugo+x start.sh
+./start.sh
+cd ..
+cd redis
+chmod ugo+x start.sh
+./start.sh
+cd ..
+
